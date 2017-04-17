38b5505e8132e768af0b0153f66e779921b23cc0
Beakman <migueldelcastillo@live.com>
2017-04-17 08:49:22 -0500

    Saved new npm packages. Removed unused modules

--- src/PlaceMarkBalloon.js
+++
@@ -1,7 +0,0 @@
-import React, {Component} from 'react';
-
-class PlaceMarkBalloon extends Component{
-
-}
-
-export default PlaceMarkBalloon;
--- src/SideBarToggle.js
+++
@@ -1,7 +0,0 @@
-import React, {Component} from 'react';
-
-class SideBarToggle extends Component{
-
-}
-
-export default SideBarToggle;
